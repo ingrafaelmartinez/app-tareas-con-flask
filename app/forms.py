98b701ceb5b165e324a8a058372a9113ea6ee864
Rafael <rafaelhitachi@gmail.com>
2021-12-23 17:39:00 -0500

    Agregando validaciones a los campos del formulario de registro

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,16 @@
 from wtforms import Form
 from wtforms import validators
 from wtforms import StringField, PasswordField
-from wtforms.fields.simple import BooleanField, EmailField
+from wtforms.fields.simple import BooleanField, EmailField, HiddenField
+
+from .models import User
+
+def codi_validator(form, field):
+    if field.data == 'codi' or field.data == 'Codi':
+        raise validators.ValidationError('El username codi no es permitido.')
+
+# def length_honeypot(form, field):
+#     raise validators.ValidationError('Solo los humanos pueden completar el registro!')
 
 class LoginForm(Form):
     username = StringField('Username', [
@@ -14,7 +23,8 @@ class LoginForm(Form):
 
 class RegisterForm(Form):
     username = StringField('Username', [
-        validators.length(min=4, max=50)
+        validators.length(min=4, max=50),
+        codi_validator
     ])
     email = EmailField('Email', [
         validators.length(min=6, max=100),
@@ -34,3 +44,20 @@ class RegisterForm(Form):
     accept = BooleanField('', [
         validators.DataRequired()
     ])
+    # honeypot = HiddenField("", [ length_honeypot ])
+
+    def validate_username(self, username):
+        if User.get_by_username(username.data):
+            raise validators.ValidationError('El username ya se encuentra en uso.')
+
+    def validate_email(self, email):
+        if User.get_by_email(email.data):
+            raise validators.ValidationError('El email ya se encuentra en uso.')
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        if len(self.password.data) < 3:
+            self.password.errors.append('El password es demasiado corto.')
+            return False
